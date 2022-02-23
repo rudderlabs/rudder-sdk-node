67efc7db3d25b008fdb0cdec356c54a9ef2de9dd
Moumita <mandal.moumita2013@gmail.com>
2022-02-23 15:24:41 +0530

    sample project added

---
+++ example/app.js
@@ -0,0 +1,70 @@
+const Rudderanalytics = require("@rudderstack/rudder-sdk-node"); // use latest package
+
+const writeKey = "WRITE_KEY"; // replace with your write-key
+const dataPlaneURL = "DATA_PLANE_URL"; // replace with your data plane url
+
+const client = new Rudderanalytics(writeKey, `${dataPlaneURL}/v1/batch`, {
+  // flushAt: 2,
+});
+/**
+ * Sample function to send 3 rudder events[identify,track,track] a make sure it's completion by promosifiying the flush
+ */
+async function test() {
+  // promisify the flush method
+  const flush = () => new Promise((resolve) => client.flush(resolve));
+
+  // call RS client methods as normal –
+  // no promisify or await needed and called concurrently
+  client.identify(
+    {
+      userId: "Test user 1",
+      traits: {
+        name: "Name Username",
+        email: "name@website.com",
+        plan: "Free",
+        friends: 21,
+      },
+    },
+    () => {
+      console.log("In identify call");
+    }
+  );
+  console.log("2");
+
+  client.track(
+    {
+      userId: "Test user 1",
+      event: "Item Purchased",
+      properties: {
+        revenue: 19.95,
+        shippingMethod: "Premium",
+      },
+    },
+    () => {
+      console.log("In track call");
+    }
+  );
+  client.track(
+    {
+      userId: "Test user 2",
+      event: "Item Viewed",
+      properties: {
+        price: 45,
+        currency: "USD",
+        productId: "Product-12345",
+      },
+    },
+    () => {
+      console.log("In track call 2");
+    }
+  );
+
+  // this both flushes and ensures completion
+  await flush();
+}
+
+test().then(() => {
+  console.log("test call done");
+});
+
+exports.Rudderanalytics = Rudderanalytics;
